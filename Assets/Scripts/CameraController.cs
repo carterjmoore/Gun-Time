33e7a3c541632820626f47bcba20e617fa878a3b
Carter Moore <cmoore12@unb.ca>
2022-02-14 00:02:38 -0400

    Implemented core movement mechanics
    Player movement, jumping, and camera controls are set up. May need to tweak gravity, speed, drag, etc. Followed this tutorial series and made some modifications: https://www.youtube.com/watch?v=LqnPeqoJRFY

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Inspiration for camera controller from the following video: https://www.youtube.com/watch?v=E5zNi_SSP_w
+//Modified by Carter Moore
+public class CameraController : MonoBehaviour
+{
+    public float sensitivity = 100f;
+
+    [SerializeField] private Transform cam;
+    [SerializeField] private Transform orientation;
+
+    private float mouseX;
+    private float mouseY;
+
+    private float multiplier = 0.01f;
+
+    private float xRotation;
+    private float yRotation;
+
+    private void Start()
+    {
+        //Hide cursor and lock it to center
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        handleInput();
+
+        //For vertical rotation, rotate the camera
+        cam.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0);
+        //For horizontal rotation, rotate the orienation game object
+        //We rotate this object instead of the player object itself to reduce jittering
+        orientation.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    private void handleInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensitivity * multiplier;
+        xRotation -= +mouseY * sensitivity * multiplier;
+
+        //Clamp our vertical cam movement so it can't look farther than straight up or down.
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Inspiration for movement controls from the following tutorial series: https://www.youtube.com/watch?v=LqnPeqoJRFY
+//Modified by Carter Moore
+public class PlayerController : MonoBehaviour
+{
+    private float playerHeight = 2f;
+
+    [Header("Movement")]
+    public float speed = 6f;
+    public float movementMultiplier = 10f;
+    public float airMultiplier = 0.4f;
+    public float jumpForce = 6.75f;
+    [Header("Drag")]
+    public float groundDrag = 6f;
+    public float airDrag = 2f;
+
+    private float horizontalMovement;
+    private float verticalMovement;
+
+    [Header("Ground Detection")]
+    [SerializeField] LayerMask groundMask;
+    private bool isGrounded;
+    private float groundDistance = 0.45f;
+
+    RaycastHit slopeHit;
+    Vector3 slopeMoveDirection;
+    
+
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+    [SerializeField] Transform orientation;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //Make sure player doesn't spin from forces
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        //Check if the player is touching the ground
+        isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, 1.1f, 0), groundDistance, groundMask);
+
+        handleInput();
+        //Keep setting drag to make sure the player doesn't slide with normal movement
+        setDrag();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jump();
+        }
+
+        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
+    }
+
+    private void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    //Handles keyboard input from the player
+    private void handleInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        //Use orientation.forward and orientation.right so direction is relative to direction player is facing
+        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
+    }
+
+    //Control drag for in-air vs on-ground movement
+    private void setDrag()
+    {
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        //Less drag is applied when in the air
+        else
+        {
+            rb.drag = airDrag;
+        }
+    }
+
+    //Handle moving the player
+    private void movePlayer()
+    {
+        if(isGrounded && !onSlope())
+        {
+            rb.AddForce(moveDirection.normalized * speed * movementMultiplier, ForceMode.Acceleration);
+        }
+        //Make it so player moves perpendicular to slope if on a slope
+        else if(isGrounded && onSlope())
+        {
+            rb.AddForce(slopeMoveDirection.normalized * speed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
+        //Make it so the player moves slower while in the air
+        else if(!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * speed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
+    }
+
+    //Handles jumping
+    private void jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    //Check if the player is on a slope
+    private bool onSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight/2 + 0.5f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+}
