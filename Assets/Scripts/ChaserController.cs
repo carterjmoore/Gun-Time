d7240f5702227aae5c6a069610ace821273e7d8e
Carter Moore <cmoore12@unb.ca>
2022-02-17 18:52:37 -0400

    Added in an enemy cube and started script

---
+++ Assets/Scripts/ChaserController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserController : ShootableEntity
+{
+    public float speed = 8f;
+    public GameObject player;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+}
