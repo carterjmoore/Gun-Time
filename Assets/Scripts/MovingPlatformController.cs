8a6947e422516529eac2eef6c5a3bf82f35241e1
Carter Moore <cmoore12@unb.ca>
2022-02-16 22:15:00 -0400

    Added ShootableEntity script
    Added a ShootableEntity script, and a ShootableEnvironment script that inherits from it. Also started working on a MovingPlatform script.

---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : ShootableEnvironment
+{
+    public float speed = 6f;
+
+    Vector3 pos1;
+    Vector3 pos2;
+    float distance;
+    float fraction;
+    bool movingBack;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        pos1 = transform.position;
+        pos2 = new Vector3(pos1.x + 20f, pos1.y, pos1.z);
+        distance = Vector3.Distance(pos1, pos2);
+        Debug.Log(distance);
+        movingBack = false;
+        fraction = 0;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        Debug.Log(fraction);
+        if(!movingBack)
+        {
+            fraction += speed / distance * Time.deltaTime;
+            fraction = Mathf.Clamp01(fraction);
+            transform.position = Vector3.Lerp(pos1, pos2, fraction);
+            if (fraction == 1) movingBack = true;
+        }
+        else
+        {
+            fraction -= speed / distance * Time.deltaTime;
+            fraction = Mathf.Clamp01(fraction);
+            transform.position = Vector3.Lerp(pos1, pos2, fraction);
+            if (fraction == 0) movingBack = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ShootableEnvironment.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootableEnvironment : ShootableEntity
+{
+    bool reducing;
+    Coroutine routine;
+
+    protected override void Start()
+    {
+        base.Start();
+        reducing = true;
+    }
+    protected virtual void Update()
+    {
+        if(timeStatus != 0 && !reducing)
+        {
+            //Start coroutine for reducing time status
+            routine = StartCoroutine(ReduceTimeStatusAgain());
+        }
+    }
+
+    IEnumerator ReduceTimeStatusAgain()
+    {
+        reducing = true;
+        yield return new WaitForSecondsRealtime(5);
+        if(timeStatus != 0)
+        {
+            //If timestatus is positive, reduce it by one, if it is negative, increase it by one
+            timeStatus = timeStatus > 0 ? timeStatus-- : timeStatus++;
+        }
+        reducing = false;
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        base.OnCollisionEnter(collision);
+        if ((collision.gameObject.CompareTag("Slow") || collision.gameObject.CompareTag("Speed")) && routine != null)
+        {
+            StopCoroutine(routine);
+            reducing = false;
+        }
+    }
+}
