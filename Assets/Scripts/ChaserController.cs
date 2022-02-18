ab995d2f76688a02addd5a1d330603f5b467ec7e
Carter Moore <cmoore12@unb.ca>
2022-02-17 20:48:00 -0400

    Improved chaser chasing
    Chaser now moves slightly past last seen pos of player

--- Assets/Scripts/ChaserController.cs
+++ Assets/Scripts/ChaserController.cs
@@ -8,51 +8,75 @@ public class ChaserController : ShootableEntity
     public GameObject player;
 
     Rigidbody rb;
-    Vector3 startPos;
-    Vector3 lastSeenPlayerPos;
     Vector3 targetPos;
+    float distanceTolerance;
+
+    bool reachedTarget;
+    bool haveAdjustedTarget;
 
     protected override void Start()
     {
         base.Start();
         rb = transform.GetComponent<Rigidbody>();
-        startPos = transform.position;
+        targetPos = transform.position;
+        distanceTolerance = 0.1f;
+        reachedTarget = true;
+        haveAdjustedTarget = true;
     }
 
     void FixedUpdate()
     {
-        Debug.Log(playerVisible());
+        //If player is visible, look at player and set targetPos to player's pos
         if (playerVisible())
         {
-            //transform.LookAt(player.transform);
             transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.y));
             targetPos = player.transform.position;
-            moveToTarget();
+            haveAdjustedTarget = false;
         }
-        else if (targetPos != null)
+        //If just lost sight of player, set targetPos to slightly ahead of its current position, to make sure chaser gets around corner
+        else if (!haveAdjustedTarget)
         {
-
+            targetPos += getTargetDir() * 0.5f;
+            haveAdjustedTarget = true;
         }
+        moveToTarget();
     }
 
     void moveToTarget()
     {
-        Vector3 targetDir = (targetPos - transform.position).normalized;
-        rb.velocity = new Vector3(targetDir.x * speed * timeMultiplier(), rb.velocity.y, targetDir.z * speed * timeMultiplier());
+        //If hasn't yet reached target, and is within the tolerance, snap to target
+        if (distanceTolerance * distanceTolerance > Vector3.Distance(transform.position, targetPos) && !reachedTarget)
+        {
+            transform.position = targetPos;
+            reachedTarget = true;
+        }
+        //If not within tolerance, set velocity towards target
+        else
+        {
+            reachedTarget = false;
+            rb.velocity = new Vector3(getTargetDir().x * speed * timeMultiplier(), rb.velocity.y, getTargetDir().z * speed * timeMultiplier());
+        }
     }
 
     //Check if player is visible to the enemy
     bool playerVisible()
     {
+        //Raycast from chaser to player to check if player is visible
         RaycastHit hit;
         Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
         if (Physics.Raycast(transform.position, player.transform.position - transform.position, out hit, Mathf.Infinity))
         {
+            //If raycast hits player before anything else, player is visible
             if(hit.transform.gameObject == player)
             {
                 return true;
             }
         }
         return false;
+    }
+
+    Vector3 getTargetDir()
+    {
+        return (targetPos - transform.position).normalized;
     }
 }
