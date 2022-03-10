a8733b6ba116c6ecbb97336993102ca728faa0a5
Carter Moore <cmoore12@unb.ca>
2022-03-09 23:05:03 -0800

    Added chaser spawner

--- Assets/Scripts/ChaserSpawnerController.cs
+++ Assets/Scripts/ChaserSpawnerController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class ChaserSpawnerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool spawnRepeatedly = false;
+    public float spawnTime = 5f;
+    public GameObject chaser;
+
+    bool spawnNow;
+    GameObject playerReference;
+
+    private void Start()
     {
-        
+        spawnNow = false;
+        GetComponent<MeshRenderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawnNow)
+        {
+            spawn();
+            StartCoroutine(waitForSpawn());
+        }
+    }
+
+    void spawn()
+    {
+        GameObject c = Instantiate(chaser, transform.position, Quaternion.identity);
+        c.GetComponent<ChaserController>().setReferences(playerReference);
+    }
+
+    public void spawnTriggered(GameObject player)
+    {
+        playerReference = player;
+        if (spawnRepeatedly) spawnNow = true;
+        else spawn();
+    }
+
+    IEnumerator waitForSpawn()
+    {
+        spawnNow = false;
+        yield return new WaitForSecondsRealtime(spawnTime);
+        spawnNow = true;
     }
 }
