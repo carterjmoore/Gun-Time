430a28e533c6cc118c5c9fda68bf4020b5761072
Carter Moore <cmoore12@unb.ca>
2022-03-09 21:26:16 -0800

    Added laser that only kills player

---
+++ Assets/Scripts/LaserPlayerKillerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPlayerKillerController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().TriggerDeath();
+        }
+    }
+}
