27c576a836dbbb418eadc924e5c0ab0278eb056d
LeumasNaib <77845008+LeumasNaib@users.noreply.github.com>
2022-03-09 22:05:01 -0400

    Death Hallway
    Long hallway with multiple shooter enemies; as you fall into the hallway chaser enemies are launched in front of you! Beware!

---
+++ Assets/Scripts/SafetyZoneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafetyZoneController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PhysEnemy")) {
+            Destroy(other.gameObject);
+        }
+
+        //Allow to fire through other enemies
+        else if (other.gameObject.CompareTag("Kill"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
