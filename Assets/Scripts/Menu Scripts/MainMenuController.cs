89d11f83f6b3a6ac84cd6b048687b312a46c77ff
Carter Moore <carterjmoore02@gmail.com>
2022-03-16 01:28:04 -0300

    Added level select menu which links to levels

--- Assets/Scripts/Menu Scripts/MainMenuController.cs
+++ Assets/Scripts/Menu Scripts/MainMenuController.cs
@@ -3,18 +3,126 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
+    //Main menu stuff
+    public TextMeshProUGUI titleText;
+    public Button levelsButton;
+    public Button optionsButton;
+    public Button quitButton;
+
+    //Level select stuff
+    public Button tutorialButton;
+    public Button chasmButton;
+    public Button gauntletButton;
+    public Button hallwayButton;
+    public Button lavaButton;
+    public Button ambushButton;
+    public Button towerButton;
+    public Button backButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowMainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
+    void disableButton(Button button)
     {
-        
+        button.image.enabled = false;
+        button.enabled = false;
+        button.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+    }
+
+    void enableButton(Button button)
+    {
+        button.image.enabled = true;
+        button.enabled = true;
+        button.GetComponentInChildren<TextMeshProUGUI>().enabled = true;
+    }
+
+    public void ShowMainMenu()
+    {
+        //Show menu stuff
+        titleText.enabled = true;
+        enableButton(levelsButton);
+        enableButton(optionsButton);
+        enableButton(quitButton);
+
+        //Hide level select stuff
+        disableButton(tutorialButton);
+        disableButton(chasmButton);
+        disableButton(gauntletButton);
+        disableButton(hallwayButton);
+        disableButton(lavaButton);
+        disableButton(ambushButton);
+        disableButton(towerButton);
+        disableButton(backButton);
+    }
+
+    public void ShowLevelSelect()
+    {
+        //Hide menu stuff
+        titleText.enabled = false;
+        disableButton(levelsButton);
+        disableButton(optionsButton);
+        disableButton(quitButton);
+
+        //Show level select stuff
+        enableButton(tutorialButton);
+        enableButton(chasmButton);
+        enableButton(gauntletButton);
+        enableButton(hallwayButton);
+        enableButton(lavaButton);
+        enableButton(ambushButton);
+        enableButton(towerButton);
+        enableButton(backButton);
+    }
+
+    public void ShowOptions()
+    {
+        //No options yet
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void LoadChasm()
+    {
+        SceneManager.LoadScene("ChasmPlatformer");
+    }
+
+    public void LoadGauntlet()
+    {
+        SceneManager.LoadScene("Gauntlet");
+    }
+
+    public void LoadHallway()
+    {
+        SceneManager.LoadScene("HallwayOfDeath");
+    }
+
+    public void LoadLava()
+    {
+        SceneManager.LoadScene("FloorIsLava");
+    }
+
+    public void LoadAmbush()
+    {
+        SceneManager.LoadScene("Ambush");
+    }
+
+    public void LoadTower()
+    {
+        SceneManager.LoadScene("Tower");
     }
 }
