49e8935b1ec2ce3fa591165a72234ae61a84b660
YourFrostyFriend <93720108+YourFrostyFriend@users.noreply.github.com>
2022-04-07 17:52:46 -0300

    dialogue implementation

--- Assets/Scripts/MapTriggers/MapTriggerController.cs
+++ Assets/Scripts/MapTriggers/MapTriggerController.cs
@@ -8,17 +8,6 @@ public class MapTriggerController : MonoBehaviour
     public GameObject object1;
     public GameObject dialogueManager;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-     
-    }
-
     private bool IsObject()
     {
         if (object1 != null)
@@ -55,6 +44,8 @@ public class MapTriggerController : MonoBehaviour
         if (this.name == "MapTrigger2" && IsObject() && other.gameObject.CompareTag("Player"))
         {
             StartCoroutine(DoorTrigger(object1, 23));
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 0, true);
+
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Alright, you've probably forgotten why we're here. First off we're stealing something.", 2, false);
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Its utility is beyond mine and your understanding, and we are gonna use it to get out of here.", 10, false);
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Here I'll open the next door for you.", 20, false);
@@ -81,12 +72,12 @@ public class MapTriggerController : MonoBehaviour
         if (this.name == "SpawnTrigger1" && other.gameObject.CompareTag("Player"))
         {
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[Note]: Red cubes are dangerous", 8, true);
-
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 16, true);
 
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Hold up, they decided to send drones!", 0, false);
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Just touching them could destroy you completely, be careful!", 8, false);
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Though they are kind of cute lookin...", 16, false);
-            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 32, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 24, false);
             this.GetComponent<BoxCollider>().enabled = false;
         }
         if (this.name == "MapTrigger4" && IsObject() && other.gameObject.CompareTag("Player"))
@@ -96,6 +87,81 @@ public class MapTriggerController : MonoBehaviour
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: I detect dozens of unfriendly faces beyond this door so stay on your toes.", 12, false);
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Don't worry I'll still be here, I wanna see that device in full action!", 20, false);
             dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: What a miraculous device we've stumbled upon...", 28, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+        }
+        if (this.name == "ChasmTrigger1" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[Note]: Collision between two projectiles nullify both projectiles.", 10, true);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 20, true);
+
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: This room looked a lot smaller in the files...", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Watch it! Those projectiles are quite unfriendly, you can stop the bullet when it collides with your own!", 6, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 14, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+
+        }
+        if (this.name == "ChasmTrigger2" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Those holo red cubes will destroy you when your body comes in contact, best to leave them untouched.", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 12, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+
+        }
+        if (this.name == "GauntletTrigger1" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: This area is used to test the functionalities of those blast robots you saw earlier.", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 12, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+
+        }
+        if (this.name == "HallwayOfDeathTrigger1" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: I'm detecting large quantities of hostiles in this area.", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: I think the best course of action to high-tail it to the door quickly!", 8, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 16, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+
+        }
+        if (this.name == "FloorIsLavaTrigger1" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Huh, this room is called 'The Lava Floor Experiment', yet I'm not seeing any natural sources of heat anywhere.", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 12, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+
+        }
+        if (this.name == "FloorIsLavaTrigger2" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Oh! I get it...", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: ...because the floor is made of lasers!", 4, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 10, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+
+        }
+        if (this.name == "AmbushTrigger1" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: This room holds significance to the facility.", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: It mass produces these hostile robots in a sickly manner.", 6, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Which also means this must be a pleasant place to be in...", 14, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 22, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+        }
+        if (this.name == "AmbushTrigger2" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: HEY! ABOVE YOU!", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 6, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+        }
+        if (this.name == "TowerTrigger1" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: This area is interesting, I hope you enjoy verticality!", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: You know I always wondered how does anyone even navigate through this place...", 10, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 20, false);
+            this.GetComponent<BoxCollider>().enabled = false;
+        }
+        if (this.name == "TowerTrigger2" && other.gameObject.CompareTag("Player"))
+        {
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("[-EEVL_I-]: Careful that room has security!", 0, false);
+            dialogueManager.GetComponent<DialogueManager>().PlayerSay("", 6, false);
             this.GetComponent<BoxCollider>().enabled = false;
         }
     }
